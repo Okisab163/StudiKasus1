8dd9374b0d1800ce4a536135b7bfb966566584eb
Muhammad Oki Angga Fairuz <Muhammad.Fairuz@atlantica.dev>
2022-01-12 15:53:04 +0700

    Adding Interface Payment Repo

---
+++ PaymentService/Data/IPaymentRepo.cs
@@ -0,0 +1,21 @@
+﻿using PaymentService.Models;
+using System.Collections.Generic;
+
+namespace PaymentService.Data
+{
+    public interface IPaymentRepo
+    {
+        bool SaveChanges();
+
+        //platforms
+        IEnumerable<Enrollment> GetAllPlatforms();
+        void CreateEnrollment(Enrollment enrollment);
+        bool EnrollmentExist(int enrollmentid);
+        bool ExternalEnrollmentExist(int externalPlatformId);
+
+        //Command
+        IEnumerable<Payment> GetPaymentsForEnrollment(int enrollmentid);
+        Payment GetPayment(int enrollmentid, int paymentId);
+        void CreatePayment(int enrollmentid, Payment payment);
+    }
+}
---
+++ PaymentService/Data/PaymentRepo.cs
@@ -0,0 +1,6 @@
+﻿namespace PaymentService.Data
+{
+    public class PaymentRepo
+    {
+    }
+}
